20432a7295bcd3c8b30c296311709b4786c8f71a
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2024-11-20 19:59:44 +0100

    tilempas and weapon added

---
+++ Assets/Scripts/Entities/Character/InputManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour, PlayerInputs.IPlayerActions
+{
+    private PlayerInputs playerInput;
+    private Rigidbody2D rb;
+    public Vector2 ipMove;
+    private Character character;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInputs();
+        playerInput.Player.SetCallbacks(this);
+        character = GetComponent<Character>();
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + character.speed * Time.deltaTime * ipMove.normalized);
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        ipMove = context.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Scripts/Weapons/AttackBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AttackBehaviour : MonoBehaviour, WeaponInputs.IWeaponActions
+{
+    private Animator animator;
+    private
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
